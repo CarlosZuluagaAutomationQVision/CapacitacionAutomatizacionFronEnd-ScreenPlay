f984d2fcd60ce3a3ff84123fa3bdda58dc71d7e2
Carlos Andrés Zuluaga Gómez <czuluaga@qvision.us>
2020-12-02 14:08:00 -0500

    Git Init

--- src/test/java/stepdefinitions/Setup.java
+++ src/test/java/stepdefinitions/Setup.java
@@ -5,17 +5,13 @@ import static net.serenitybdd.screenplay.actors.OnStage.theActorCalled;
 import io.cucumber.java.Before;
 import net.serenitybdd.screenplay.actors.OnStage;
 import net.serenitybdd.screenplay.actors.OnlineCast;
-import net.thucydides.core.annotations.Managed;
-import org.openqa.selenium.WebDriver;
 
 public class Setup {
 
   @Before
-  public void configurarActor(){
+  public void configurarActor() {
     OnStage.setTheStage(new OnlineCast());
     theActorCalled("Carlos");
   }
 
-  @Managed(driver = "chrome", uniqueSession = true)
-  WebDriver driver;
 }
